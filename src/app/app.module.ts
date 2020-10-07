bf4111607567413c45d84186e90ffc8f40ab5860
Yuri Daza <yvdaza@bancolombia.com.co>
2020-10-07 18:50:26 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { DetailModule } from './detail/detail.module';
+import { SharedModule } from './shared/shared.module';
+import { BookingModule } from './booking/booking.module';
+import { SigninModule } from './signin/signin.module';
+import { SignupModule } from './signup/signup.module';
+import { ErrorPageModule } from './error-page/error-page.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    DetailModule,
+    SharedModule,
+    BookingModule,
+    SigninModule,
+    SignupModule,
+    ErrorPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
